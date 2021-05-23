ec0afd5a79d8995a3fa05108578edb2ae9fa45f4
Sascha Wildgrube <sascha@wildgrube.com>
2021-05-23 20:45:11 +0200

    The Header page no longer has the view tab - thus allowing to edit the header with less clicks

--- www/_source/wdk/shell/beanc.sh
+++
@@ -1,1 +0,0 @@
-cat /proc/user_beancounters
---
+++ www/_source/wdk/shell/filesys-cleanup.sh
@@ -0,0 +1,5 @@
+rm -v /var/lib/psa/dumps/domains/*/backup_logs*
+rm -v /var/log/syslog.*
+rm -v /var/log/auth.log.*
+rm -v /var/lib/psa/dumps/domains/*/backup_*
+rm -v /var/lib/psa/dumps/domains/*/databases/*/*
